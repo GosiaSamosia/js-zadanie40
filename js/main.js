d74dfe83316bd672d6d73d36d8e758f319f77ca1
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-17 12:16:23 +0200

    FORMULARZ!

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.querySelector('button').onclick = function () {
+
+    var name = document.getElementById("name");
+    console.log(name.value);
+
+    var surname = document.getElementById("surname");
+    console.log(surname.value);
+
+    var phone = document.getElementById("phone");
+    console.log(phone.value);
+}
+
+
+/*var newbutton =
+document.querySelector('button').onclick = function () {
+    name.textContent = document.querySelector ('f-name');
+
+    var name = document.getElementById('p-name');
+    console.log(name.value);
+    name.textContent = (name.value);
+}
+
+
+var name = document.querySelector('p-name');
+name.textContent = "Małgorzata";
+
+
+var name = "Małgorzata";
+var surname = "Osińska";
+var phone = "123-456-789";
+
+document.querySelector('p').onclick = function(name.textContent);
+*/
