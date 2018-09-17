986dbdcebdde2ac71776ec5fd099c6da6106377a
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-17 13:07:13 +0200

    FORMULARZ z css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
+var przyciskwyswietl = document.querySelector("newbutton");
+
 document.querySelector('button').onclick = function () {
 
     var name = document.getElementById("name");
     console.log(name.value);
-
+    
     var surname = document.getElementById("surname");
     console.log(surname.value);
 
     var phone = document.getElementById("phone");
     console.log(phone.value);
+
+    container.textContent = ("Dane:" + " " + name.value + " " + surname.value + " " + phone.value);
 }
 
 
-/*var newbutton =
+/*
+document.querySelector('button').onclick = function () {
+var paragrafname = document.getElementById("name");
+console.log(name.value);
+paragrafname.textContent = (name.value);
+    
+var paragrafsurname = document.getElementById("surname");
+console.log(surname.value);
+    paragrafsurname.textContent = (surname.value);
+
+var paragrafphone = document.getElementById("phone");
+console.log(phone.value);
+    paragrafphone.textContent = (phone.value);
+
+var przyciskwyswietl = document.querySelector("newbutton");
+
+}
+
+
+
+    var paragrafname = document.getElementById("name");
+    console.log(name.value);
+    
+    var paragrafsurname = document.getElementById("surname");
+    console.log(surname.value);
+
+    var paragrafphone = document.getElementById("phone");
+    console.log(phone.value);
+
+container.textContent = ("name.value" + " " + "surname.value" + " " + "phone.value");
+
+
+}
+
+document.name.name.value;
+document.surname.surname.value;
+document.phone.phone.value;
+
+var newbutton =
 document.querySelector('button').onclick = function () {
     name.textContent = document.querySelector ('f-name');
 
